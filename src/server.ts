3008a323d666fdc47fa5998ed37e48cb2ccefd46
PedroRafael13 <pedrorafaeldsn12@gmail.com>
2025-02-01 21:58:25 -0400

    config at mongoose

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
+import "dotenv/config"
 import express, { json } from "express";
 import { router } from "./routes";
+import { setupMongo } from "./database";
 
 const app = express();
 
-app.use(json());
-app.use(router)
+setupMongo().then(() => {
+  app.use(json());
+  app.use(router)
 
-app.listen(3333, () => console.log("App is running at port 3333!"))
+  app.listen(3333, () => console.log("App is running at port 3333!"))
+})
